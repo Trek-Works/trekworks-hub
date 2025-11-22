19c6058ce55ddc84d8c78a1ffdec763877148ee4
Trek-Works <github.travel@gmail.com>
2025-11-22 19:57:05 +1100

    Add files via upload

--- public-pwa/JP/GJN-2024-Sep/flights/service-worker.js
+++ public-pwa/JP/GJN-2024-Sep/flights/service-worker.js
@@ -1,41 +1,40 @@
-const CACHE_NAME = "flights-2024-cache-v1";
+const CACHE_NAME = "jp-flights-2024-v1";
 
-const BASE_PATH = "/public-pwa/JP/GJN-2024-Sep/flights/";
-
-const ASSETS = [
-  `${BASE_PATH}`,
-  `${BASE_PATH}index.html`,
-  `${BASE_PATH}offline.html`,
-  `${BASE_PATH}manifest.json`,
-  `${BASE_PATH}assets/icons/icon-192.png`,
-  `${BASE_PATH}assets/icons/icon-512.png`
+const STATIC_ASSETS = [
+    "./",
+    "./index.html",
+    "./offline.html",
+    "./manifest.json",
+    "./assets/icons/icon-192x192.png",
+    "./assets/icons/icon-512x512.png"
 ];
 
 // Install
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_ASSETS))
+    );
 });
 
 // Activate
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.map(key => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            }))
+        )
+    );
 });
 
 // Fetch
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request).then(
-        res => res || caches.match(`${BASE_PATH}offline.html`)
-      )
-    )
-  );
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            return cachedResponse ||
+                fetch(event.request).catch(() =>
+                    caches.match("./offline.html")
+                );
+        })
+    );
 });
